ca9121f1389a9ef30addc54183fbb27b53104c1b
Anthony Lariccia <anthony.lariccia@bsmac1675.bestcorp.net>
2025-06-18 12:02:54 -0400

    POST Route setup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import axios from "axios";
 import path from "path";
 import { fileURLToPath } from "url";
+import { chown } from "fs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
@@ -17,9 +18,33 @@ app.set("views", path.join(__dirname, "views"));
 
 // Home route
 app.get("/", (req, res) => {
-  res.render("index", { weather: null, error: null });
+  res.render("index.ejs", { weather: null, error: null });
 });
 
+//POST route
+app.post("/", async (req, res) => {
+  const city = req.body.city;
+  const geoCacheAPIURL = `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(
+    city
+  )}&language=en&format=json`;
+  try {
+    const geoResponse = await axios.get(geoCacheAPIURL);
+    const results = geoResponse.data.results;
+    if (!results || results.length === 0) {
+      return res.render("index", { weather: null, error: "City not found." });
+    }
+    let chosenResult = results[0];
+    let cityLat = chosenResult.latitude;
+    let cityLon = chosenResult.longitude;
+    console.log(cityLat, cityLon);
+    res.render("index", { weather: "Still working on this", error: null });
+  } catch (error) {
+    res.render("index", {
+      weather: null,
+      error: "City not found or API error.",
+    });
+  }
+});
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
